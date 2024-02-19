39c4de827c7b4aabd1cadc4753915058bd9ade30
thecarpetjasp <71789855+thecarpetjasp@users.noreply.github.com>
2024-02-19 05:34:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import re
 
 input("")
 
@@ -16,13 +17,26 @@ driver.get("https://www.alphacyprus.com.cy/live")
 cookies = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "cookiescript_accept")))
 cookies.click()
 
-video_element = driver.find_element(By.ID, "rmpPlayer")
-button = driver.find_element(By.CSS_SELECTOR, 'button[class="rmp-fullscreen rmp-button rmp-i rmp-i-resize-full"]')
+#video_element = driver.find_element(By.ID, "rmpPlayer")
+video_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "rmpPlayer")))
+button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[class="rmp-fullscreen rmp-button rmp-i rmp-i-resize-full"]')))
+#button = driver.find_element(By.CSS_SELECTOR, 'button[class="rmp-fullscreen rmp-button rmp-i rmp-i-resize-full"]')
+#button2 = driver.find_element(By.CSS_SELECTOR, 'button[class="rmp-button rmp-volume rmp-i rmp-i-off-volume"]')
+button2 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[class="rmp-button rmp-volume rmp-i rmp-i-off-volume"]')))
 
 actions = ActionChains(driver)
 
+actions.move_to_element(button2).perform()
+time.sleep(1)
+volume = driver.find_element(By.CSS_SELECTOR, 'div[class="rmp-volume-indicator rmp-color-bg"]')
+volume = re.search(r'\d+', volume.text).group(0)
+if int(volume) == 0:
+    button2.click()
+
+
 actions.move_to_element(video_element).perform()
 button.click()
+
 
 time.sleep(100)
 input("")
